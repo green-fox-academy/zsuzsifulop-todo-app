d52f1e6bbd2d749d7f7b4a947a22cc2a3d7261ed
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-09-29 11:21:34 +0200

    feat(to_do.py): new function names + error handling

--- to_do.py
+++ to_do.py
@@ -1,21 +1,25 @@
 import sys
+
 def what_to_do():
     print("Tell me what shoud I do \n a - add a new element \n l - print me the elements \n r - read the elements \n c - check if an elements is ready")
-def controller():    
-    if sys.argv[-1] == "to_do_application.py":
+
+def controller():
+    if sys.argv[-1] == "to_do.py":
         what_to_do()
     if sys.argv[-1] == "-l":
         print_out()
     elif sys.argv[-2] == "-a":
-        add_todo(sys.argv[-1])
-    elif sys.argv[-2] =="-r":
+        add_line(sys.argv[-1])
+    elif sys.argv[-2] == "-r":
         remove_line(int(sys.argv[-1]))
     elif sys.argv[-2] == "-c":
-         check_okey(int(sys.argv[-1]))
+        check_complete(int(sys.argv[-1]))
+    else:
+        print("unsupport argument")
+        what_to_do()
    
 
-
-def open_and_interpret():
+def list_the_items():
     file = open("to_do_list.txt", 'r')
     text = file.readlines()
     file.close()
@@ -31,13 +35,11 @@ def open_and_interpret():
         dictionary["task"] = things
         todos.append(dictionary) #hozzádja a listát a dictionaryhez! először
     return todos
-    
 
-todos = open_and_interpret()
 
 #add todo here
-def add_todo(todos_text):
-    todos = open_and_interpret() #megnyitjuk az előző def-et
+def add_line(todos_text):
+    todos = list_the_items() #megnyitjuk az előző def-et
     if todos != []:
         todos[-1]['task'] = todos[-1]['task'][0:] + "\n" #a -1 sor elé beszúrunk egy spacet
     todo_dict = {"complete" : False, "task": todos_text}
@@ -58,8 +60,9 @@ def write_out_file(todos):
     text_to_write.write(text_to_file)
     text_to_write.close()
 
+
 def print_out():
-    todos = open_and_interpret()
+    todos = list_the_items()
     print_text = ""
     if todos == []:
         print("No todos for today :(")
@@ -75,21 +78,25 @@ def print_out():
             i += 1
         print(print_text)
 
-def check_okey(number_of_item):
-    todos = open_and_interpret()
+
+def check_complete(number_of_item):
+    todos = list_the_items()
     todos[number_of_item-1]['complete'] = True #eddig False volt, most átállítjuk true-ra!
     write_out_file(todos)
 
+
 def remove_line(number_of_items):
-    todos = open_and_interpret()
-    del todos[number_of_items-1] #egész sor megy a kukába!
-    write_out_file(todos)
-# what_to_do()
-# add_todo("két nagyon hülye teve teve")
-# print_out()
-# check_okey(10)
-# print_out()
-# remove_line(10)
-# print_out()
+    todos = list_the_items()
+    if number_of_items == 20:
+        print("unable to remove: no index provided")
+    elif number_of_items == 20:
+        for elements in todos:
+            del elements
+        write_out_file(todos)
+    elif number_of_items == str(number_of_items):
+        print("unable to remove: index is not a bound")
+    else:
+        del todos[number_of_items-1] #egész sor megy a kukába!
+        write_out_file(todos)
 
 controller()
